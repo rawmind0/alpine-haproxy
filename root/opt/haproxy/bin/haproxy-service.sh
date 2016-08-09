be5e49f83b1dfa2b3a802fb1cab20a15829802bd
Raul Sanchez <rawmind@gmail.com>
2016-08-09 23:38:30 +0200

    Updated haporxy to v1.6.7

---
+++ root/opt/haproxy/bin/haproxy-service.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+function log {
+        echo `date` $ME - $@
+}
+
+function serviceLog {
+}
+
+function serviceStart {
+    log "[ Starting ${SERVICE_NAME}... ]"
+    haproxy -f ${SERVICE_CONF} -p ${SERVICE_HOME}/haproxy.pid -sf $(cat ${HAPROXY_HOME}/haproxy.pid)
+}
+
+function serviceStop {
+    log "[ Stoping ${SERVICE_NAME}... ]"
+    kill -15 $(cat ${SERVICE_HOME}/haproxy.pid)
+}
+
+function serviceRestart {
+    log "[ Restarting ${SERVICE_NAME}... ]"
+    serviceStop
+    serviceStart
+    /opt/monit/bin/monit reload
+}
+
+case "$1" in
+        "start")
+            serviceStart &>> /proc/1/fd/1
+        ;;
+        "stop")
+            serviceStop &>> /proc/1/fd/1
+        ;;
+        "restart")
+            serviceRestart &>> /proc/1/fd/1
+        ;;
+        *) echo "Usage: $0 restart|start|stop"
+        ;;
+
+esac
+
